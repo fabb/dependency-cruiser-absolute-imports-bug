3666c3410335807e45e754e99f962fea4b71456f
fabb <153960+fabb@users.noreply.github.com>
2021-01-20 10:41:39 +0100

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { X } from "src/X"
+
+const Index = () => (
+    <div>
+        <h1>This shows absolute import errors with dependency-cruiser</h1>
+        <X />
+    </div>
+)
+
+export default Index
---
+++ src/X.tsx
@@ -0,0 +1,3 @@
+export const X = () => {
+    return <div>In the codesandbox, go to the Terminal (not console), open a bash with the "+" and execute "npm run depcruise"</div>
+}
